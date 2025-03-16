9d7b9663043168eeb3f9110fd84eac94ed369474
muthiazzh <muthiahazzahra200@gmail.com>
2025-03-16 20:01:54 +0700

    menambahkan class DataGeneric

--- tpmodul5_103022330117/Program.cs
+++ tpmodul5_103022330117/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿//using System;
+
+//public class HaloGeneric
+//{
+//   
+//    public void SapaUser<T>(T user)
+//    {
+//        Console.WriteLine($"Halo user {user}");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        HaloGeneric halo = new HaloGeneric();
+//        halo.SapaUser("Muthi");
+//    }
+//}
+
+using System;
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataGeneric<string> nimData = new DataGeneric<string>("103022330117");
+
+        nimData.PrintData();
+    }
+}
